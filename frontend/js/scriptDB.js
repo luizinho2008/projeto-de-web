5ce7a15750b314e1ee1e5dfc7312bd4c197808b0
teste <teste@example.com>
2024-10-08 14:23:59 -0300

    Rota e função de deleção de torcedores no banco de dados

--- frontend/js/scriptDB.js
+++ frontend/js/scriptDB.js
@@ -20,6 +20,10 @@ const exibeTorcedores = (resposta) => {
         html += `<div class='torcedor-item'>
                     <img src='${torcedor.imagem}' class='torcedores-img' alt='imagem torcedor'>
                     <h2 class='torcedor-info'>(Nome: ${torcedor.nome}) (Telefone: ${torcedor.telefone}) (Email: ${torcedor.email})</h2>
+                    <div class='buttons'>
+                        <button class='deleta' onclick='deletaTorcedor(${torcedor.id})'>Deletar</button> <br> <br>
+                        <button class='edita' onclick='editaTorcedor(${torcedor.id})'>Editar</button>
+                    </div>    
                 </div>`;
     });
     document.getElementById("lista").innerHTML = html;
@@ -65,4 +69,33 @@ const gravaTorcedor = (event) => {
             document.getElementById("grava-falha").style.display = "none";
         }, 2500);
     });
+};
+
+const deletaTorcedor = (id) => {
+    const decisao = confirm("Você realmente deseja apagar esse torcedor?");
+
+    if(decisao) {
+        const api = `http://localhost:8000/api/torcedores/${id}`;
+        fetch(api, {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer your-token-here',
+            }
+        })
+        .then((response) => {
+        if (!response.ok) {
+            throw new Error('Erro na requisição');
+        }
+        return response.json();
+        })
+        .then((data) => {
+            console.log('Sucesso:', data);
+            carregaTorcedores();
+        })
+        .catch((error) => {
+            console.error('Erro:', error);
+            carregaTorcedores();
+        });
+    }
 };
