5cee5caaf5ef46b566fd3f864303716cd4607bc5
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2024-12-20 09:11:53 -0300

    1000%

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,24 +1,24 @@
 const mysql = require("mysql");
 
-const host = "localhost";
-const user = "root";
-const password = "";
-const database = "SEP";
+const hostname = "i3rw1.h.filess.io";
+const database = "palmeirasDB2024_leaveplate";
+const port = "3306";
+const username = "palmeirasDB2024_leaveplate";
+const password = "581111afb4d97b71fa30091e28097bfabe82c448";
 
 const connexionDB = mysql.createConnection({
-    host: host,
-    user: user,
+    host: hostname,
+    user: username,
     password: password,
-    database: database
+    database: database,
+    port: port,
 });
 
 connexionDB.connect((erro) => {
-    if (erro) {
-        console.log(`Falha ao se conectar com o MySQL: ${erro}`);
+    if(erro) {
+        console.log(`Falha ao se conectar com o banco de dados`);
     }
     else {
-        console.log(`Conexão efetuada com sucesso`);
+        console.log(`Conexão efetuada com sucesso com o MySQL`);
     }
-});
-
-module.exports = connexionDB;
+})
