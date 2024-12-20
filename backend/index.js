6b1d16ebf7276d65e9abaee5a654eb2981a5dea8
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2024-12-20 09:21:25 -0300

    1000%

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("./config/config");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(cors());
